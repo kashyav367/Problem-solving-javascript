1ffe24974c3ba5b8569b1242b438f98def1d1f42
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-09-10 20:20:19 +0530

    palindrome solved

--- 01-js/medium/palindrome.js
+++ 01-js/medium/palindrome.js
@@ -7,8 +7,24 @@
 */
 
 function isPalindrome(str) {
+    let left=0; 
+    let right = str.length-1;
+
+    while(left < right){
+       if(str[left] !== str[right]){
+         return false;
+       }
+       else{
+        left++;
+       right--;
+       }
+    }
     return true;
 }
+
+let str = "racecar"
+let result = isPalindrome(str);
+console.log(result);
 
 module.exports = isPalindrome;
 
