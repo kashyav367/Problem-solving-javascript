dfc945dfc9ec887736a538265a07e786a827b847
Ankit kumar Singh <ankit12030621028@gmail.com>
2026-09-14 12:49:36 +0530

    times.js

--- 01-js/medium/times.js
+++ 01-js/medium/times.js
@@ -8,5 +8,17 @@ Hint - use Date class exposed in JS
 */
 
 function calculateTime(n) {
-    return 0.01;
+    // return 0.01;
+    const date = new Date();
+    let sum = 0;
+    for(let i=1 ; i<=n; i++){
+        sum +=i
+    }
+    return date.getSeconds(sum);
 }
+
+
+let result = calculateTime(100000);
+console.log(result)
+
+
